2204a23bff59931e346746cab546d4e9b5dbb9d8
Wagner J Goncalves <wagner@tuneduc.com.br>
2018-09-21 13:43:43 -0300

    refactor: account service balance method
    Refactor balance mehtod at account service by using the transactions association from account model.

--- app/services/account_service.rb
+++ app/services/account_service.rb
@@ -1,12 +1,10 @@
 class AccountService
   def self.balance(account_id)
-    return {
-      status: :error,
-      message: 'Invalid account'
-    } unless Account.exists?(account_id)
+    account = Account.find_by(id: account_id)
 
-    transactions = Transaction.select('sum(amount) as total')
-                              .where(account_id: account_id)
+    return render_invalid_account_error unless account
+
+    transactions = account.transactions.select('sum(amount) as total')
     debits = transactions.debit&.first&.total || 0
     credits = transactions.credit&.first&.total || 0
 
@@ -22,5 +20,14 @@ class AccountService
     return true if response[:status] == :ok && response[:balance] >= amount
 
     false
+  end
+
+  class << self
+    def render_invalid_account_error
+      {
+        status: :error,
+        message: 'Invalid account'
+      }
+    end
   end
 end
